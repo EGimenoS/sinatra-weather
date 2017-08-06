16249e5a6c5b76dbfc956730bd2a5f1c2c52a070
EgimenoS <ergisan@gmail.com>
2017-08-06 22:33:08 +0000

    temp displayed for a city, connected with open Weather api

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
+require 'config_env'
+require 'httparty'
+ConfigEnv.init("#{__dir__}/config/env.rb")
 
 set :port, 3000
 
 get '/' do
-  'Hello World'
+  resp = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Valencia,ES&APPID=#{ENV['OW_KEY']}")
+  data= JSON.parse(resp.body) 
+  @temp = (data['main']['temp'] - 273.15).round(2)
+  erb :index
 end
