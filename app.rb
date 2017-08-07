0d6889bf26377c40034d5fc4c485e8e6265a5e3f
EgimenoS <ergisan@gmail.com>
2017-08-07 16:57:25 +0000

    added redis support to store requests in cache for 5 mins

--- app.rb
+++ app.rb
@@ -2,12 +2,24 @@ require 'sinatra'
 require 'config_env'
 require_relative 'models/weather.rb'
 require 'httparty'
+require 'uri'
+require 'redis'
 ConfigEnv.init("#{__dir__}/config/env.rb")
 
+REDIS = Redis.new(url: ENV['REDIS_URL'])
 set :port, 3000
 
 get '/' do
-  weather = Weather.new
+  @cities = [
+    "Valencia,ES",
+    "Ojos Negros,ES",
+    "Almussafes,ES",
+    "Madrid,ES"
+  ]
+
+  @city = params[:city] || "Valencia,ES"
+  weather = Weather.new(@city)
   @temp = weather.temp
+  @icons = weather.icons
   erb :index
 end
--- models/weather.rb
+++ models/weather.rb
@@ -1,8 +1,31 @@
+require 'httparty'
+require 'uri'
+
 class Weather
+  def initialize(city)
+    @city = city
+  end
 
   def weather_data
-    resp = HTTParty.get("http://api.openweathermap.org/data/2.5/weather?q=Valencia,ES&APPID=#{ENV['OW_KEY']}")
-    JSON.parse(resp.body) 
+    if ! REDIS.get(@city).nil?
+      return JSON.parse(REDIS.get(@city))
+    else
+      resp = HTTParty.get(
+        "http://api.openweathermap.org/data/2.5/weather?q=#{URI.escape(@city)}&APPID=#{ENV['OW_KEY']}"
+      )
+      REDIS.set(@city, resp.body)
+      REDIS.expire @city, 300 #seconds
+      
+      JSON.parse(resp.body) 
+    end
+  end
+
+  def icons
+    data = weather_data
+    return [] if data.nil? || data['weather'].nil?
+    data['weather'].collect do |weather|
+      weather['icon'] + ".png"
+    end.uniq
   end
 
   def temp
